8ffdfee6115226f4823612ae8627ce7b53711a7d
toriumi <saori.toriumi.54@gmail.com>
2019-04-26 20:23:30 +0900

    Add comment description

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
 package main
 
+// main パッケージの中にある fmt 関数
+// 使う関数しかインポートしちゃだめ
 import "fmt"
 
-func main()  {
+// fmt パッケージ内に main 関数がある
+// main は必ず一番最初に読み込まれる関数
+func main() {
 	// p は大文字
 	// なんのモジュールを使ってるか書く
+	// 外部からパッケージをインポートした場合、パッケージと関数の間にピリオドを入れなければならない
 	fmt.Println("Hello Golang!")
 }
